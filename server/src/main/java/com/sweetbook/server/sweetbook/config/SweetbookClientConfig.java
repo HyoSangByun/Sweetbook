e11b005546c035f683fdc77d88bb767e596fe36d
HyoSangByun <billy04100410@gmail.com>
2026-04-08 00:06:27 +0900

    feat(sweetbook): disable SSL verification for Sweetbook RestClient

--- server/src/main/java/com/sweetbook/server/sweetbook/config/SweetbookClientConfig.java
+++ server/src/main/java/com/sweetbook/server/sweetbook/config/SweetbookClientConfig.java
@@ -4,14 +4,23 @@ import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpHeaders;
 import org.springframework.web.client.RestClient;
+import lombok.extern.slf4j.Slf4j;
+
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+import java.security.SecureRandom;
+import java.security.cert.X509Certificate;
 
 @Configuration
+@Slf4j
 public class SweetbookClientConfig {
 
     @Bean
     public RestClient sweetbookRestClient(SweetbookProperties properties) {
+        disableSslVerificationGlobally();
         return RestClient.builder()
                 .baseUrl(properties.baseUrl())
                 .requestInterceptor((request, body, execution) -> {
@@ -28,5 +37,33 @@ public class SweetbookClientConfig {
         requestFactory.setConnectTimeout((int) properties.connectTimeout().toMillis());
         requestFactory.setReadTimeout((int) properties.readTimeout().toMillis());
         return requestFactory;
+    }
+
+    private void disableSslVerificationGlobally() {
+        try {
+            TrustManager[] trustAllCerts = new TrustManager[]{
+                    new X509TrustManager() {
+                        @Override
+                        public X509Certificate[] getAcceptedIssuers() {
+                            return new X509Certificate[0];
+                        }
+
+                        @Override
+                        public void checkClientTrusted(X509Certificate[] chain, String authType) {
+                        }
+
+                        @Override
+                        public void checkServerTrusted(X509Certificate[] chain, String authType) {
+                        }
+                    }
+            };
+
+            SSLContext sslContext = SSLContext.getInstance("TLS");
+            sslContext.init(null, trustAllCerts, new SecureRandom());
+            HttpsURLConnection.setDefaultSSLSocketFactory(sslContext.getSocketFactory());
+            HttpsURLConnection.setDefaultHostnameVerifier((hostname, session) -> true);
+        } catch (Exception exception) {
+            log.warn("Failed to disable SSL verification for Sweetbook RestClient. reason={}", exception.getMessage(), exception);
+        }
     }
 }
